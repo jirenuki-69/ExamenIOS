fe46e5988e1ec831b80551a1dfda4730eaddbd28
Miguel Fuentes <mfuentesmodelo@gmail.com>
2022-05-12 15:58:08 -0700

    Initial Commit

---
+++ ExamenIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExamenIOS
+//
+//  Created by Miguel Fuentes on 12/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
