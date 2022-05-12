aea29b03b9f7b082a43452e851c70a369633228b
Miguel Fuentes <mfuentesmodelo@gmail.com>
2022-05-12 16:04:43 -0700

    Hay commit

--- ExamenIOS/ViewController.swift
+++ ExamenIOS/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Roger es gei")
         // Do any additional setup after loading the view.
     }
 
